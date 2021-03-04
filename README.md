83f4408b46202fad65899ff3f84528a819f97f4b
santiago-rodrig <santo1996.29@gmail.com>
2021-03-03 21:27:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# popcount
+
+Go library that provides functions for calculating
+the population count of an uint64 value.
+
+> The Go Programming Language, Alan A. A. Donovan and Brian W. Kernighan
+
+## Setup
+
+```sh
+go get github.com/santiago-rodrig/popcount
+```
---
+++ popcount.go
@@ -0,0 +1,24 @@
+// Package popcount defines a function for calculating
+// the population count of a uint64 value
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// Calculate returnes the population count for
+// a uint64 value
+func Calculate(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}

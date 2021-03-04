c8395a30f0981daa6066d28ab8555a56950b1f47
santiago-rodrig <santo1996.29@gmail.com>
2021-03-03 22:02:34 -0400

    better implementation using bit shifting

--- popcount.go
+++ popcount.go
@@ -2,23 +2,16 @@
 // the population count of a uint64 value
 package popcount
 
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
 // Calculate returnes the population count for
 // a uint64 value
 func Calculate(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
+	bits := 0
+
+	for i := 0; i < 64; i++ {
+		if x&(1<<i) != 0 {
+			bits++
+		}
+	}
+
+	return bits
 }
